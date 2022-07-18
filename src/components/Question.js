958886991823dc31cbd6c140278f77e6b231bc9a
Vincent Langat <kimutailangat033@gmail.com>
2022-07-18 10:30:19 +0300

    Done.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return; 
+    }
+
+    //this uses setTimeout to run a callback function after 1 second.
+  
+    const timerId = setTimeout(() => {
+      
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000); //decrements the time by 1 second.
+
+//implementing a clean up function
+    return function () {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered]);
+  // useEffect(() => {}, [variable1, variable2]): 
+  //Dependencies array with elements in it we want to run the effect every time timeRemaining changes
+  // Run the side effect any time the variable(s) change
+  // onAnswered is also a dependency, even though it doesn't change
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
